e4cc3b345e0bf0c799742485ebd03eb6e870f484
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-06-29 17:02:58 +0200

    updated python code example

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,31 @@
-# pip install neo4j-driver
-# python example.py
+# pip install neo4j
 
-from neo4j import GraphDatabase, basic_auth
+from neo4j import (
+    GraphDatabase,
+    basic_auth,
+)
 
-driver = GraphDatabase.driver(
-    "bolt://<HOST>:<BOLTPORT>", 
-    auth=basic_auth("<USERNAME>", "<PASSWORD>"))
-session = driver.session()
+scheme = "neo4j"
+port = 7687
+host_name = "example.com"
+user_name = "example_user"
+user_password = "example_password"
+
+uri = "{scheme}://{host_name}:{port}".format(scheme=scheme, host_name=host_name, port=port)
+
+driver = GraphDatabase.driver(uri, auth=basic_auth(user_name, user_password))
 
 cypher_query = '''
 MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
 WHERE movie.title IN $favorites
-RETURN rec.title as title, count(*) as freq 
+RETURN rec.title as title, count(*) as freq
 ORDER BY freq DESC LIMIT 5
 '''
 
-results = session.run(cypher_query,
-  parameters={"favorites":["The Matrix"]})
+with driver.session() as session:
+    result = session.run(cypher_query, parameters={"favorites": ["The Matrix"]})
 
-for record in results:
-  print(record['title'])
+    for record in result:
+        print("title: {} freq: {}".format(record["title"], record["freq"]))
+
+driver.close()
