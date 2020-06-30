d28f4b34bf13835e299e5540e6fe748ac411d7b7
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-06-30 12:01:19 +0200

    updated the python code example

--- code/python/example.py
+++ code/python/example.py
@@ -15,17 +15,31 @@ uri = "{scheme}://{host_name}:{port}".format(scheme=scheme, host_name=host_name,
 
 driver = GraphDatabase.driver(uri, auth=basic_auth(user_name, user_password))
 
-cypher_query = '''
-MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
-WHERE movie.title IN $favorites
-RETURN rec.title as title, count(*) as freq
-ORDER BY freq DESC LIMIT 5
-'''
+def get_recomended_movies(transaction, favorites):
+    # this is a transaction function,
+    # this function can be retried by the transaction manager
 
-with driver.session() as session:
-    result = session.run(cypher_query, parameters={"favorites": ["The Matrix"]})
+    cypher_query = '''
+    MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie)
+    WHERE movie.title IN $favorites
+    RETURN rec.title as title, count(*) as freq
+    ORDER BY freq DESC LIMIT 5
+    '''
+
+    result = transaction.run(cypher_query, parameters={"favorites": favorites})
+
+    movies = []
 
     for record in result:
-        print("title: {} freq: {}".format(record["title"], record["freq"]))
+        # the cypher_query defined the RETURN to have the keys "title" and "freq"
+        movies.append(record["title"])
+
+    return movies
+
+with driver.session() as session:
+    movies = session.read_transaction(get_recomended_movies, favorites=["The Matrix"])
+
+    for movie in movies:
+        print(movie)
 
 driver.close()
