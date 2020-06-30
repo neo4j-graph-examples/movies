f18732f90a09ad8918758288d4e9cbdabfa715e7
Gregory Woods <gregory.woods@neo4j.com>
2020-06-30 11:54:06 +0100

    Update java homepage example

--- code/java/Example.java
+++ code/java/Example.java
@@ -6,25 +6,22 @@ import org.neo4j.driver.*;
 import static org.neo4j.driver.Values.parameters;
 
 import java.util.List;
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonMap;
+import static java.util.Collections.singletonList;
 
 public class Example {
 
     public static void main(String...args) {
-        Config noSSL = Config.builder().withoutEncryption().build();
-        try (Driver driver = GraphDatabase.driver("bolt://<HOST>:<BOLTPORT>",AuthTokens.basic("<USERNAME>","<PASSWORD>"),noSSL);
-		     Session session = driver.session()) {
+        try (Driver driver = GraphDatabase.driver("neo4j://<HOST>:7687",AuthTokens.basic("<HOST>","<PASSWORD>"));
+             Session session = driver.session()) {
             String cypherQuery =
-                "MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) "+
-                "WHERE movie.title IN $favorites "+
-                "RETURN rec.title as title, count(*) as freq "+
-                "ORDER BY freq DESC LIMIT 5";
-            Result result = session.run(cypherQuery, parameters("favorites",asList("The Matrix")));
-            while (result.hasNext()) {
-                System.out.println(result.next().get("title").asString());
-            }
-	    }
+                    "MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) "+
+                    "WHERE movie.title IN $favorites "+
+                    "RETURN rec.title as title, count(*) as freq "+
+                    "ORDER BY freq DESC LIMIT 5";
+            List<Record> records =
+                    session.readTransaction(tx -> tx.run(cypherQuery, parameters("favorites", singletonList( "The Matrix" ))).list());
+            records.forEach(record -> System.out.println( record.get( "title" ).asString()));
+        }
     }
 }
 
