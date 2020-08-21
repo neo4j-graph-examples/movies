8508a527d8aa1c261b0978d1d5b3156d4ac8328e
Michael Hunger <github@jexp.de>
2020-08-21 02:47:52 +0200

    Update Indent

--- code/csharp/Example.cs
+++ code/csharp/Example.cs
@@ -9,31 +9,32 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Neo4j.Driver;
-	
+  
 namespace dotnet {
-    class Example {
-	static async Task Main() {
-		var driver = GraphDatabase.Driver("bolt://<HOST>:<BOLTPORT>", 
-						AuthTokens.Basic("<USERNAME>", "<PASSWORD>"));
+  class Example {
+  static async Task Main() {
+    var driver = GraphDatabase.Driver("bolt://<HOST>:<BOLTPORT>", 
+                    AuthTokens.Basic("<USERNAME>", "<PASSWORD>"));
 
-		var cypherQuery =
-				@"
-				MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) 
-				WHERE movie.title = $favorite 
-				RETURN rec.title as title, count(*) as freq 
-				ORDER BY freq DESC LIMIT 5 
-				";
+    var cypherQuery =
+      @"
+      MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) 
+      WHERE movie.title = $favorite 
+      RETURN rec.title as title, count(*) as freq 
+      ORDER BY freq DESC LIMIT 5 
+      ";
 
-		var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
-		var result = await session.ReadTransactionAsync(async tx =>
-		{
-			var r = await tx.RunAsync(cypherQuery, 
-			                new { favorite="The Matrix"});
-			return await r.ToListAsync();
-		});
-		await session?.CloseAsync();
-		foreach (var row in result)
-			Console.WriteLine(row["title"].As<string>());
-		}
-	}
+    var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
+    var result = await session.ReadTransactionAsync(async tx => {
+      var r = await tx.RunAsync(cypherQuery, 
+              new { favorite="The Matrix"});
+      return await r.ToListAsync();
+    });
+
+    await session?.CloseAsync();
+    foreach (var row in result)
+      Console.WriteLine(row["title"].As<string>());
+	  
+    }
+  }
 }
--- code/java/Example.java
+++ code/java/Example.java
@@ -9,28 +9,30 @@ import static org.neo4j.driver.Values.parameters;
 
 public class Example {
 
-    public static void main(String...args) {
-        Driver driver = GraphDatabase.driver("bolt://<HOST>:<BOLTPORT>",
-                           AuthTokens.basic("<USERNAME>","<PASSWORD>"));
-        try (Session session = driver.session(SessionConfig.forDatabase("neo4j"))) {
+  public static void main(String...args) {
 
-            String cypherQuery =
-                "MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) " +
-                "WHERE movie.title = $favorite " +
-                "RETURN rec.title as title, count(*) as freq " +
-                "ORDER BY freq DESC LIMIT 5 " ;
+    Driver driver = GraphDatabase.driver("bolt://<HOST>:<BOLTPORT>",
+              AuthTokens.basic("<USERNAME>","<PASSWORD>"));
 
-            var result = session.readTransaction(
-                tx -> tx.run(cypherQuery, 
-                             parameters("favorite","The Matrix"))
-                        .list());
+    try (Session session = driver.session(SessionConfig.forDatabase("neo4j"))) {
 
-            for (Record record : result) {
-                System.out.println(record.get("title").asString());
-            }
-        }
-        driver.close();
+      String cypherQuery =
+        "MATCH (movie:Movie)<-[:ACTED_IN]-(actor)-[:ACTED_IN]->(rec:Movie) " +
+        "WHERE movie.title = $favorite " +
+        "RETURN rec.title as title, count(*) as freq " +
+        "ORDER BY freq DESC LIMIT 5 " ;
+
+      var result = session.readTransaction(
+        tx -> tx.run(cypherQuery, 
+                parameters("favorite","The Matrix"))
+            .list());
+
+      for (Record record : result) {
+        System.out.println(record.get("title").asString());
+      }
     }
+    driver.close();
+  }
 }
 
 
